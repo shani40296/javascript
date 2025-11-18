7ec411f55d675f7697ee7d61000b3c20b5304730
Shani Patel <shanipatel37780@gmail.com>
2025-11-18 23:59:03 +0530

    variables and data
    learning time

---
+++ 1 Variables and Datatype/variable&datatype.js
@@ -0,0 +1,56 @@
+/*var age =25;
+if(true){
+    console.log(age)
+}*/
+ 
+/*function solve(){
+    var age = 25;
+    console.log(age)
+}
+solve();*/
+
+//{
+//let a = 10;
+//console.log(a)
+//}
+
+ //let a = 20;
+ // a = 30;
+
+//  let a = 10;
+//  a = "shani";
+//  a = true;
+//  a = null;
+// const a = 28;
+// console.log(a);
+
+
+// DATA TYPE USE
+
+// const a = 20;
+// console.log(a)
+// let marks = 20; 
+// marks = "shani"
+// marks = true;
+// console.log(marks);
+
+// let marks;
+//console.log(marks);
+
+// let value = null;
+// console.log(value)
+// OR
+// let marks = null;
+// console.log(marks)
+
+let marks = 12345678901234567890234567891234567890
+console.log(marks);
+
+marks = "shani";
+marks = 12;
+marks = null;
+console.log(typeof(marks));
+
+const sym1 = Symbol('description1');
+const sym2 = Symbol('description2');
+console.log(sym1 === sym2);
