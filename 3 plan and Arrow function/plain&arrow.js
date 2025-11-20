227e3ba2aed6764ed00e9811a44fda5cc0f3a23a
Shani Patel <shanipatel37780@gmail.com>
2025-11-20 20:57:54 +0530

    loop and function
    basic learning

---
+++ 3 plan and Arrow function/plain&arrow.js
@@ -0,0 +1,52 @@
+//  function defination....
+// function seyMyName(){
+//     console.log("shani patel")
+// }
+//  seyMyName(); // function use - function call
+
+//... function printCounting(){
+// for( let i=1; i<=100; i++){
+//     console.log(i)
+// }
+// }
+// printCounting();
+
+//... function printName(num){
+//     console.log("printing Number:", num)
+// }
+// printName(5);
+
+//... function getAverage(num1,num2){
+//     let avg =(num1 + num2)/2;
+//     console.log("average",avg)
+// }
+// getAverage(3,7)
+
+// return function....
+// function getsum(a,b,c){
+//     let sum =a+b+c;
+//     return sum;
+// }
+// let ans = getsum(1,2,3);
+// console.log("printing Sum:", ans)
+
+//... function getMyName(firstName,fullName){
+//     let name = firstName + " " + fullName;
+//     return name;
+// }
+// let ans = getMyName("shani", "patel")
+// console.log(ans,"my full Name")
+
+//multiplication...
+// function getmultiplication(a,b){
+//     return a*b;
+// }
+// console.log(getmultiplication(2,10));
+
+// squareNumber .....
+// let squareNumber = function(num){
+// let ans = num**2;
+// return ans;
+// }
+// let ans = squareNumber(5);
+// console.log(ans)
