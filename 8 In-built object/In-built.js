57269215c07fa1abe943d707acdff5c4fe1a3b6f
Shani Patel <shanipatel37780@gmail.com>
2025-11-29 20:58:39 +0530

    In-built object
    basic learning time

---
+++ 8 In-built object/In-built.js
@@ -0,0 +1,30 @@
+// console.log("shani")
+
+// console.log(Math.PI)
+
+// console.log(Math.max(60,30,24,600,712,89))
+
+// console.log(Math.min(60,30,24,600,712,89))
+
+// console.log(Math.round(1.))
+
+// console.log(Math.floor(1.4))
+
+// console.log(Math.abs(-5))
+
+// console.log(Math.random())
+
+// console.log(Math.sqrt(5)) // matlab underroot 5
+// console.log(Math.pow(3,10)) // 3 ko 10 bar multiplai karna
+
+// let curr = new Date();
+// console.log(curr)
+// OR
+let curr = new Date();
+let date = new Date('jun 20 1998 07:15');
+let newdate = new Date (1998, 5, 20, 7);
+//console.log(newdate);
+
+// console.log(newdate.getFullYear());
+console.log(newdate.setFullYear(2001));
+console.log(newdate)
